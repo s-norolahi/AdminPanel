86611584886decc8703372fb94ce92c2354d32fd
SinaRiyahi <sina.riyahi76@gmail.com>
2023-02-04 15:06:32 +0330

    Order & Product

--- AdminPanel/Domain/Entities/OrderFeature.cs
+++ AdminPanel/Domain/Entities/OrderFeature.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Domain.Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class OrderFeature
-    {
-    }
+    public class OrderFeature : IEntity
+        {
+            public int ID { get; set; }
+
+            public int OrderID { get; set; }
+
+            [Required]
+            [StringLength(200)]
+            public string Name { get; set; }
+
+            public Order Order { get; set; }
+        }
 }
--- AdminPanel/Domain/Entities/ProductFeature.cs
+++ AdminPanel/Domain/Entities/ProductFeature.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Domain.Common;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class ProductFeature
+    public class ProductFeature : IEntity
     {
+        public int ID { get; set; }
+
+        public int ProductID { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        public Product Product { get; set; }
+
     }
 }

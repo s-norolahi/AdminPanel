0689dc9bd8724eb7eeb1700f317f22cbdbdc6063
SinaRiyahi <sina.riyahi76@gmail.com>
2023-02-08 21:00:37 +0330

    PageDto

--- AdminPanel/Domain/Models/Dto/OrderPageDto.cs
+++ AdminPanel/Domain/Models/Dto/OrderPageDto.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using AutoMapper;
+using Domain.Common;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models.Dto
 {
-    internal class OrderPageDto
+    public class OrderPageDto
     {
+        public ShippingMethod ShippingMethod { get; set; }
+        public ICollection<OrderFeature> OrderFeatures;
+        public ICollection<OrderDetail> Details { get; set; }
+        public ICollection<Invoice> Invoices { get; set; }
+    }
+    public class OrderGridView : IHaveCustomMapping
+    {
+        public long ID { get; set; }
+        public string ApplicationUserID { get; set; }
+        public int ShippingMethodID { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string Note { get; set; }
+        public decimal ShippingMethodPrice { get; set; }
+        public void CreateMappings(Profile profile)
+        {
+            var mappingExpression = profile.CreateMap<OrderGridView, Order>();
+            mappingExpression.ReverseMap();
+            //CustomMappings(mappingExpression.ReverseMap());
+        }
+
     }
 }
+
--- AdminPanel/Domain/Models/Dto/ProductPageDto.cs
+++ AdminPanel/Domain/Models/Dto/ProductPageDto.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using AutoMapper;
+using Domain.Common;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models.Dto
 {
-    internal class ProductPageDto
+    public class ProductPageDto
     {
+        public Producer Producer { get; set; }
+        public Category Category { get; set; }
+        public Subcategory Subcategory { get; set; }
+        public ICollection<ProductFeature> ProductFeatures { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<Discount> Discounts { get; set; }
+    }
+    public class ProductGridView : IHaveCustomMapping
+    {
+        public long ID { get; set; }
+        public int ProducerID { get; set; }
+        public int CategoryID { get; set; }
+        public int SubcategoryID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int AvailableQuantity { get; set; }
+
+        public void CreateMappings(Profile profile)
+        {
+            var mappingExpression = profile.CreateMap<ProductGridView, Product>();
+            mappingExpression.ReverseMap();
+            //CustomMappings(mappingExpression.ReverseMap());
+        }
+
     }
 }

89714a72f1d67fd3df22e236d41e202b7090478a
SinaRiyahi <sina.riyahi76@gmail.com>
2023-02-12 23:57:11 +0330

    ProductController

--- AdminPanel/WebAPi/Controllers/v1/ProductController.cs
+++ AdminPanel/WebAPi/Controllers/v1/ProductController.cs
@@ -38,36 +38,6 @@ namespace WebAPi.Controllers.v1
         {
             return Ok(await _productRepository.FindProductAsync(id, cancellationToken));
         }
-        //[HttpGet("{id:required}")]
-        //public Product Index(long id)
-        //{
-        //    var result = productRepository.GetProduct(id);
-        //    result.Producer = productRepository.GetProducerForProduct(id);
-        //    result.Category = productRepository.GetCategoryForProduct(id);
-        //    result.Subcategory = productRepository.GetSubcategoryForProduct(id);
-        //    result.Discounts = productRepository.GetDiscountsForProduct(id);
-
-        //    return result;
-        //}
-
-        //[HttpGet("selected")]
-        //public List<Product> GetSelectedProducts([FromQuery] int[] ids)
-        //{
-        //    var results = ids?.Select(id => productRepository.GetProduct(id)).ToList();
-
-        //    foreach (var product in results)
-        //    {
-        //        product.Producer = productRepository.GetProducerForProduct(product.ID);
-        //    }
-
-        //    return results;
-        //}
-
-        //[HttpGet("discounts")]
-        //public ICollection<Discount> Discounts()
-        //{
-        //    return discountRepository.GetAllDiscounts();
-        //}
     }
 }
 
